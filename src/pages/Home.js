4114ba6c13216257344a3d86039785a86e74c8f4
Sayem92 <sayemmiha123@gmail.com>
2023-02-13 12:02:43 +0600

    load data ans show

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('products.json')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, []);
+
+ 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+
+      {
+        products?.map((product) => <ProductCard
+          key={product._id}
+          product={product}
+        ></ProductCard>)
+      }
     </div>
   );
 };
